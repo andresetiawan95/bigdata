504531d8638733704416d7d22042d4afc9f4ca99
Andre Setiawan <Andre Setiawan>
2016-05-16 15:03:32 +0700

    TF-IDF

--- TFIDF/TFIDF.py
+++ TFIDF/TFIDF.py
@@ -1,27 +1,30 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.feature import IDF
-import cobainput
+import sys
+
 #boilerplate
+inputan = sys.argv[1]
 conf = SparkConf().setMaster("local").setAppName("SparkTFIDF")
 sc = SparkContext(conf = conf)
 
-cobaInputan = cobainput.inputan()
+#cobaInputan = cobainput.inputan()
 
 #load documents (one per line)
 hashing = HashingTF(100000)
-fields = sc.textFile("subset-small.tsv").map(lambda x: x.split("\t"))
-documents = fields.map(lambda x: x[3].split(" "))
-namaDocument = fields.map(lambda x: x[1])
+fields = sc.textFile("berita.txt").map(lambda x: x.split("\t"))
+documents = fields.map(lambda x: x[2].lower().split(" "))
+namaDocument = fields.map(lambda x: x[0])
+
 
 tf = hashing.transform(documents).cache();
 idf = IDF(minDocFreq=2).fit(tf);
 tfidf = idf.transform(tf)
 
-keywordTF = hashing.transform([cobaInputan])
+keywordTF = hashing.transform([inputan.lower()])
 keywordHashValue = int(keywordTF.indices[0])
 keywordRelevance = tfidf.map(lambda x: x[keywordHashValue])
 zipped = keywordRelevance.zip(namaDocument)
 
 print "Dokumen yang cukup relevan untuk keyword tersebut adalah "
-print zipped.max()
+print zipped.max()[1]
